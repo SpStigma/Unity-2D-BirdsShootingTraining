46dba888d8320ddc96e03711a4f486de60451447
Rivet Yoann <134633530+SpStigma@users.noreply.github.com>
2024-05-26 17:32:40 +0200

    Add: Toggle group and create one more scene.

--- Assets/Scenes/Scripts/MainMenu.cs
+++ Assets/Scenes/Scripts/MainMenu.cs
@@ -1,13 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Toggle main1;
+    public Toggle main2;
+    public Toggle main3;
+    public Toggle main4;
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (main1.isOn)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        }
+        else if (main2.isOn)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+        }
+        else if(main3.isOn)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+        }
+        else if(main4.isOn)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 5);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
     }
 
     public void QuitGame()
